c75ca3c25593094cc98cedfcbbc988cf7b89c0c1
citra <citralestari9februari@gmail.com>
2025-09-30 21:04:41 +0800

    hero section blm jadi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                'xs': '300px',
+                'sm': '640px',
+                'md': '768px',
+                'lg': '1024px',
+                'xl': '1280px',
+                '2xl': '1536px',
+                '3xl': '1700px',
+                '4xl': '2000px',
+            },
+            colors: {
+                'white': '#F8F8F8',
+                'primary': '#2C7BD8',
+                'secondary': '#7ED6DF',
+                'black': '#222222',
+            },
+            fontFamily: {
+                'pop': ['Poppins'],
+                'open': ['Open Sans'],
+            },
+        },
     },
     plugins: [],
 };
